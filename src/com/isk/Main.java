ed1887da5fe3222859bb3f57ca98d644f17ff40a
imal <imalsisira@gmail.com>
2018-10-09 21:08:52 -0700

    sysout

--- src/com/isk/Main.java
+++ src/com/isk/Main.java
@@ -4,7 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.print("dsadasdsadsadssdsfsdf");
-        System.out.println("sddsadsad");
+        System.out.println("sample project");
+        System.out.println("push");
+        Main mm = new Main();
+        String value = mm.returnValue();
+        System.out.println(value);
+    }
+
+    public String returnValue() {
+        String myvalue = "this is text";
+        return myvalue;
     }
 }
